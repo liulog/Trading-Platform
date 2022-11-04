70e284eccd94c20ac2b86585282883c9c8eb5e27
liujingyu <2537738252@qq.com>
2022-11-04 15:34:25 +0800

    Init all

---
+++ Back-end/src/main/java/com/hust/tradingplatform/TradingPlatformApplication.java
@@ -0,0 +1,14 @@
+package com.hust.tradingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingPlatformApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TradingPlatformApplication.class, args);
+    System.out.println("Hello !");
+  }
+
+}
